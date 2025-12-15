de7a8789f713cb91fd68aca0968a3f86b6b1ddbd
jho0202 <144909339+jho0202@users.noreply.github.com>
2025-12-15 16:09:03 +0800

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,8 @@
 <?php 
-define('DB_HOST','library-management-system2-wasmer.app');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','library');
-
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+$db   = getenv("DB_NAME");
 try
 {
 $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
