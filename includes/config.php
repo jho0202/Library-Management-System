d344f4c8e6335a718ff64013fbd4c0582bcd7d3b
jho0202 <144909339+jho0202@users.noreply.github.com>
2025-12-15 15:50:56 +0800

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,5 +1,5 @@
 <?php 
-define('DB_HOST','library-management-system-e5v7.onrender.com');
+define('DB_HOST','library-management-system-wasmer.app');
 define('DB_USER','root');
 define('DB_PASS','');
 define('DB_NAME','library');
